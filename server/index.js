8db7b557501b82d5915a75f7f5f749eb30b21e4c
siriAyanur <meemusiri@gmail.com>
2021-07-28 10:34:14 +0530

    changed uri

--- server/index.js
+++ server/index.js
@@ -20,7 +20,6 @@ app.use('/channel', channelRoutes);
 app.use('/', (req, res) => {
     res.send('Hello to insights application')
 })
-const CONNECTION_URL = 'mongodb+srv://user:user@cluster0.lzaah.mongodb.net/Memories?retryWrites=true&w=majority'
  
 const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.CONNECTION_URL, { useUnifiedTopology: true, useNewUrlParser: true,useFindAndModify:false })
